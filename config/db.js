491ebf0c2015b140d2fb9894fc2199520eb91d33
Louie Alfonso <lfriasalfonso@gmail.com>
2024-06-18 12:24:36 +0100

    MERN Project Start

---
+++ config/db.js
@@ -0,0 +1,19 @@
+// db.js
+
+const mongoose = require("mongoose");
+const db =
+  "mongodb+srv://admindev:87TupR5SvUlNtFY8@mernstack1.9i4s6yp.mongodb.net/?retryWrites=true&w=majority&appName=MernStack1";
+/* Replace <password> with your database password */
+
+mongoose.set("strictQuery", true, "useNewUrlParser", true);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("Yehey! MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
